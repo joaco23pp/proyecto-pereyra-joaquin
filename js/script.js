972680ac1374ef0934320f95ac3fe859fe5ae3dd
joaquin pereyra <joacopp@outlook.com>
2023-12-25 21:39:42 -0300

    subir archivo

--- js/script.js
+++ js/script.js
@@ -1,30 +1,46 @@
-// Función para calcular el porcentaje de una cantidad
-function calcularPorcentaje(porcentaje, cantidad) {
-  return (porcentaje * cantidad) / 100;
+// Clase para representar una operación
+class Operacion {
+  constructor(porcentaje, cantidad) {
+    this.porcentaje = porcentaje;
+    this.cantidad = cantidad;
+  }
+
+  calcularPorcentaje() {
+     return (this.porcentaje * this.cantidad) / 100;
+  }
 }
-// Función para ejecutar el calculador
+
+ // Función para ejecutar el calculador
 function ejecutarCalculador() {
-let continuar = true;
-let porcentaje, cantidad;
+  let continuar = true;
+  let operaciones = [];
 
-while (continuar) {
-    // Leer el porcentaje y la cantidad desde el usuario
-    porcentaje = parseInt(prompt("Introduce el porcentaje (0-100):"));
-    cantidad = parseInt(prompt("Introduce la cantidad:"));
+  while (continuar) {
+     // Leer el porcentaje y la cantidad desde el usuario
+    let porcentaje = parseInt(prompt("Introduce el porcentaje (0-100):"));
+    let cantidad = parseInt(prompt("Introduce la cantidad:"));
 
-    // Verificar si el usuario quiere salir
+     // Crear una nueva operación
+    let operacion = new Operacion(porcentaje, cantidad);
+
+     // Agregar la operación al array
+    operaciones.push(operacion);
+
+     // Verificar si el usuario quiere salir
     let respuesta = prompt(
-    "Deseas realizar otra operación? (Si/No):"
+      "Deseas realizar otra operación? (Si/No):"
     ).toUpperCase();
     if (respuesta === "No") {
-    continuar = false;
+      continuar = false;
     }
+  }
 
-    // Calcular el porcentaje y mostrar el resultado
-    let resultado = calcularPorcentaje(porcentaje, cantidad);
-    console.log(`El ${porcentaje}% de ${cantidad} es: ${resultado}`);
-}
+  // Calcular el porcentaje y mostrar el resultado
+  for (let i = 0; i < operaciones.length; i++) {
+    let resultado = operaciones[i].calcularPorcentaje();
+    console.log(`El ${operaciones[i].porcentaje}% de ${operaciones[i].cantidad} es: ${resultado}`);
+  }
 }
 
-// Llamar a la función para ejecutar el calculador
+ // Llamar a la función para ejecutar el calculador
 ejecutarCalculador();
